dcdf7dea1412ffc393f0590137b3ad27287a69bd
Otavio Macedo <otaviomacedo@gmail.com>
2021-02-27 18:30:17 +0000

    Improved the documentation of the factory

--- src/main/scala/proofs/regex/UniversalDecider.scala
+++ src/main/scala/proofs/regex/UniversalDecider.scala
@@ -9,7 +9,15 @@ trait Factory extends ((String => Boolean, String) => (String => Boolean))
 /**
  * This class does an impossible task: given a machine and input, it decides
  * whether the machine accepts the input
- * @param factory A special factory blablabla
+ * @param factory A factory receives two parameters, a function and a string,
+ *                and returns another function. Implementations of this trait
+ *                should fulfill the following contract:
+ *                   * If the function in the first parameter accepts the
+ *                   string in the second parameter, the resulting function
+ *                   (the factory’s product, so to speak) recognizes some
+ *                   regular language. It doesn’t matter which language.
+ *                   * Otherwise, the resulting function does not accept
+ *                   any regular language.
  */
 class UniversalDecider(factory: Factory) extends ((String => Boolean, String) => Boolean) {
   override def apply(fn: (String => Boolean), input: String): Boolean =
